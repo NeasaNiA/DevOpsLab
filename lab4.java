17f47f091a087bbdb10e3b330a194a7d64d97dfb
NeasaNiA <72471534+NeasaNiA@users.noreply.github.com>
2021-02-27 10:05:33 +0000

    first commit

---
+++ lab4.java
@@ -0,0 +1,19 @@
+package lab4project;
+
+public class lab4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		final int NUM_FACTS = 100;
+		for(int i = 0; i < NUM_FACTS; i++)
+			System.out.println( i + "! is " + factorial(i));
+	}
+	
+	public static int factorial(int n)
+	{	int result = 1;
+		for(int i = 2; i <= n; i++)
+			result *= i;
+		return result;
+	}
+
+}
